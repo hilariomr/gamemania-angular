d41e2549cefaa366d61144e25f53105cdd8760f2
Marcos Hilario <mrhilario1985@gmail.com>
2022-02-09 11:48:55 -0300

    adicionando Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcessorioComponent } from './views/acessorio/acessorio.component';
+import { GameComponent } from './views/game/game.component';
+import { HomeComponent } from './views/home/home.component';
+import { JogoComponent } from './views/jogo/jogo.component';
+import { LancamentoComponent } from './views/lancamento/lancamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "acessorio", component: AcessorioComponent},
+  { path: "lancamento", component: LancamentoComponent},
+  { path: "game", component: GameComponent},
+  { path: "jogo", component: JogoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { AcessorioComponent } from './views/acessorio/acessorio.component';
+import { LancamentoComponent } from './views/lancamento/lancamento.component';
+import { GameComponent } from './views/game/game.component';
+import { JogoComponent } from './views/jogo/jogo.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AcessorioComponent,
+    LancamentoComponent,
+    GameComponent,
+    JogoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
